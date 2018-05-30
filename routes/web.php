a2cfadd685c6792976106e35ed1be3b36dd5e90c
unknown <sonnguyend10s@gmail.com>
2018-05-30 20:16:46 +0700

    update route project

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,6 @@ Route::post('/generate2faSecret','PasswordSecurityController@generate2faSecret')
 Route::post('/2fa','PasswordSecurityController@enable2fa')->name('enable2fa');
 Route::post('/disable2fa','PasswordSecurityController@disable2fa')->name('disable2fa');
 
-Route::post('/verify2FA','HomeController@verify2FA');
-
 Route::post('/2faVerify', function () {
     return redirect(request()->session()->get('_previous')['url']);
 })->name('2faVerify')->middleware('2fa');
