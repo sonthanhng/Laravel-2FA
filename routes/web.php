43de62a53371ee22940e4fc848e667967786d463
unknown <sonnguyend10s@gmail.com>
2018-05-30 16:18:01 +0700

    add 2FA to login, complete project

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,9 @@ Route::get('/2fa','PasswordSecurityController@show2faForm');
 Route::post('/generate2faSecret','PasswordSecurityController@generate2faSecret')->name('generate2faSecret');
 Route::post('/2fa','PasswordSecurityController@enable2fa')->name('enable2fa');
 Route::post('/disable2fa','PasswordSecurityController@disable2fa')->name('disable2fa');
+
+Route::post('/verify2FA','HomeController@verify2FA');
+
+Route::post('/2faVerify', function () {
+    return redirect(request()->session()->get('_previous')['url']);
+})->name('2faVerify')->middleware('2fa');
